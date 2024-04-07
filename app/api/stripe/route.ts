52014b8125689e41390fd44b128d4c35e172c631
Aytijha Chakraborty <achakraborty@paktolus.com>
2024-04-07 20:40:26 +0530

    fix

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -6,6 +6,7 @@ import { stripe } from "@/lib/stripe";
 import { absoluteUrl } from "@/lib/utils";
 
 const settingsUrl = absoluteUrl("/settings");
+export const dynamic = 'no';
 
 export async function GET() {
   try {
