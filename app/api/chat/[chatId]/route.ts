a5fed911866c205317c9b2f8fcb54f8237fa117e
Aytijha Chakraborty <achakraborty@paktolus.com>
2024-05-15 14:55:01 +0530

    user checks

--- app/api/chat/[chatId]/route.ts
+++ app/api/chat/[chatId]/route.ts
@@ -20,7 +20,7 @@ export async function POST(
     const { prompt } = await request.json();
     const user = await currentUser();
 
-    if (!user || !user.firstName || !user.id) {
+    if (!user) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
--- app/api/companion/route.ts
+++ app/api/companion/route.ts
@@ -10,9 +10,9 @@ export async function POST(req: Request) {
     const user = await currentUser();
     const { src, name, description, instructions, seed, categoryId } = body;
 
-    console.log(user);
+    // console.log(user);
 
-    if (!user || !user.id || !user.firstName) {
+    if (!user) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
@@ -30,7 +30,7 @@ export async function POST(req: Request) {
       data: {
         categoryId,
         userId: user.id,
-        userName: user.firstName,
+        userName: user.firstName || user.username || "User",
         src,
         name,
         description,
